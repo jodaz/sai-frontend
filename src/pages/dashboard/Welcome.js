b8a21a7cbcff07d684e4c115e3de0dc962523c34
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-06-10 01:17:34 -0400

    Add home

--- src/pages/dashboard/Welcome.js
+++ src/pages/dashboard/Welcome.js
@@ -1,17 +1,29 @@
 import Box from '@mui/material/Box'
+import dtiLogo from '../../images/DTI2.png'
+import mayorLogo from '../../images/mayorLogo.png'
 
 const Welcome = () => {
     return (
         <Box sx={{
             fontSize: '2rem',
             display: 'flex',
-            backgroundColor: theme => theme.palette.primary.main,
+            backgroundColor: theme => theme.palette.secondary.main,
             width: '100%',
-            color: theme => theme.palette.secondary.main, 
-            padding: '1rem'
+            padding: '2rem 1rem',
+            flexDirection: 'column'
         }}>
-            <Box>
+            <Box width='100%'>
                 ¡Bienvenido al Sistema de Atención Integral!
+            </Box>
+            <Box sx={{
+                paddingTop: '2rem',
+                width: '40%',
+                display: 'flex',
+                justifyContent: 'space-between',
+                flexDirection: 'row'
+            }}>
+                <img height='50rem' src={dtiLogo} />
+                <img height='75rem' src={mayorLogo} />
             </Box>
         </Box>
     )
